38dd32d5bc6f3072d17ccd9e2847d41d51fce8cc
tanujn45 <tanujn45@gmail.com>
2024-07-04 16:02:34 -0400

    First commit: My NeoVim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,38 @@
+-- function ColorMyPencils(color)
+-- 	color = color or "rose-pine"
+-- 	vim.cmd.colorscheme(color)
+--
+-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				-- disable_background = true,
+				styles = {
+                    italic = false,
+				},
+			})
+			-- vim.cmd.colorscheme("rose-pine")
+			-- ColorMyPencils()
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				commentStyle = {
+					italic = false,
+				},
+				keywordStyle = {
+					italic = false,
+				},
+			})
+			vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "c",
+                "cpp",
+                "rust",
+                "python",
+                "typescript",
+                "java",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "elixir",
+                "heex",
+                "javascript",
+                "html",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
+}
