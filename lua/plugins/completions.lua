90c5eeb471e506c2395584018e24b4f69ea6e567
tanujn45 <tanujn45@gmail.com>
2024-07-06 16:06:52 -0400

    Feature: Open and close Neotree with same key binding (<C-n>)

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -29,7 +29,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete(),
 					-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
