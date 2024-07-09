d5b1166cfbecc5e5cc330e996b066dfbf7ce88fa
tanujn45 <tanujn45@gmail.com>
2024-07-09 11:03:44 -0400

    Update: Not using eslint_d due to errors

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
+				-- require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
